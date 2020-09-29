de30bc56a2a761281c2f33b7230bfc21f1f18ab4
Aadarsh <aadarsh-s@outlook.com>
2020-09-29 22:00:39 +0530

    removing models

--- src/models/product.ts
+++ src/models/product.ts
@@ -51,32 +51,4 @@ class Product {
   }
 }
 
-// @Entity()
-// export class Product extends BaseEntity {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column('varchar', { nullable: false, length: 100 })
-//   title: string;
-
-//   @Column('numeric', { nullable: false })
-//   price: number;
-
-//   @Column('text', { nullable: false })
-//   imageUrl: string;
-
-//   @Column('varchar', { nullable: false, length: 255 })
-//   description: string;
-
-//   @OneToMany(() => CartItem, cItem => cItem.prodid)
-//   cItem: CartItem[];
-
-//   @OneToMany(() => OrderItem, oItem => oItem.prodid)
-//   oItem: OrderItem[];
-
-//   @ManyToOne(() => User, user => user.prodId, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
-//   @JoinColumn({ referencedColumnName: 'id', name: 'userid' })
-//   userid: User;
-// }
-
 export default module.exports = Product;
